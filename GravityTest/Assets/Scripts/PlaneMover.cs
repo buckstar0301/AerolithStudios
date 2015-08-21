423ce7dcbf750d18bbb0d90f1c00896fb9999a85
dunks184 <duncan.g.barr@gmail.com>
2015-08-21 19:57:10 +0930

    Added Unity Project

---
+++ GravityTest/Assets/Scripts/PlaneMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneMover : MonoBehaviour {
+
+	public float speed;
+	public float respawnTrigger;
+	public float respawnDist;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += Vector3.forward * -speed;
+
+		if (transform.position.z < respawnTrigger) {
+			transform.position -= Vector3.forward * respawnDist;
+		}
+
+	
+	}
+}
